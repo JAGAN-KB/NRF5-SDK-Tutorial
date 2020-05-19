c4bdbde12061e0d5850e94cd9ba96954b34f77a8
JAGAN-KB <jagan.kb38@gmail.com>
2020-05-19 19:41:42 +0530

    Initial commit

---
+++ gcc + vscode example/helloworld.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    cout<< "Enter 2 numbers to added\n";
+    int a,b,c;
+    cin>> a >> b;
+    c = a+b;
+    cout << "The sum of the 2 numbers is:" <<c << "\n";
+    return 0;
+}
